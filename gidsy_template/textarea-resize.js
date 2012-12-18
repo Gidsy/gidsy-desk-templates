561fc27bf3e5c16cd99cf146e4913469559ec674
Julien Donck <julien@gidsy.com>
2012-12-18 19:54:20 +0100

    added auto resize to the contact page on desk

---
+++ gidsy_template/textarea-resize.js
@@ -0,0 +1,30 @@
+function makeExpandingArea(container) {
+ var area = container.querySelector('textarea');
+ var span = container.querySelector('span');
+ if (area.addEventListener) {
+   area.addEventListener('input', function() {
+     span.textContent = area.value;
+   }, false);
+   span.textContent = area.value;
+ } else if (area.attachEvent) {
+   // IE8 compatibility
+   area.attachEvent('onpropertychange', function() {
+     span.innerText = area.value;
+   });
+   span.innerText = area.value;
+ }
+ // Enable extra CSS
+ container.className += ' active';
+}
+
+var areas = document.querySelectorAll('.expandingArea');
+var l = areas.length;
+
+while (l--) {
+ makeExpandingArea(areas[l]);
+}
+
+
+setInterval(function () {
+  $('.expandingArea').toggleClass('invalid', $('.expandingArea textarea').hasClass('invalid'));
+}, 100);
